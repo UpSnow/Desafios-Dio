cf232b12fb924cbb9e242e34742c60d1631ad791
UpSnow <arthurcel2002@gmail.com>
2025-11-23 06:03:16 -0300

    ✔

---
+++ classes-de-um-Jogo/src/classesJogo.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt-sync")();
+const ataques = {
+    Guerreiro: "Espada",
+    Mago: "Magia",
+    Monge: "Artes Marciais",
+    Ninja: "Shuriken"
+};
+class personagem {
+    constructor(nome, idade, tipoPersonagem) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipoPersonagem = tipoPersonagem;
+    }
+    ataque() {
+        if (this.tipoPersonagem === "Guerreiro") {
+            console.log(`${this.tipoPersonagem} atacou usando ${ataques.Guerreiro}`);
+        } else if (this.tipoPersonagem === "Mago") {
+            console.log(`${this.tipoPersonagem} atacou usando ${ataques.Mago} `);
+        } else if (this.tipoPersonagem === "Monge") {
+            console.log(`${this.tipoPersonagem} atacou usando artes ${ataques.Monge} `);
+        } else if (this.tipoPersonagem === "Ninja") {
+            console.log(`${this.tipoPersonagem} atacou usando  ${ataques.Ninja}`);
+        }
+    }
+}
+let quantidadePersonagens = Number(prompt("Quantos personagens deseja criar? "));
+for (let i = 1; i <= quantidadePersonagens; i++) {
+
+    console.log(`\n--- Personagem ${i} ---`);
+    let nome = prompt("Digite o nome do personagem: ");
+    let idade = prompt("Digite a idade do personagem: ");
+    let tipoPersonagem = prompt("Digite o tipo do personagem (Guerreiro, Mago, Monge, Ninja): ").toLowerCase();
+
+    const meuPersonagem = new personagem(nome, idade, tipoPersonagem);
+    meuPersonagem.ataque();
+}
+
+
